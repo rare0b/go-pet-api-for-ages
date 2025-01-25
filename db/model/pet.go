a64e51b057e9bcc496e5080ed5293779e2903605
rare0b <rare0b0110@gmail.com>
2025-01-25 21:07:03 +0900

    chore: format

--- db/model/pet.go
+++ db/model/pet.go
@@ -6,19 +6,19 @@ import (
 
 type Pet struct {
 	gorm.Model
-	Name string
-	Status string
+	Name      string
+	Status    string
 	PhotoUrls []PhotoUrl
 }
 
 type PhotoUrl struct {
 	gorm.Model
 	PetId uint
-	Url string
+	Url   string
 }
 
 type Tag struct {
 	gorm.Model
 	PetId uint
-	Name string
+	Name  string
 }
