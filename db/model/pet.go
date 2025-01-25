281db6559856fce72a110b6379a519cb0d9c22c5
rare0b <rare0b0110@gmail.com>
2025-01-25 20:54:05 +0900

    feat: pet model definition

---
+++ db/model/pet.go
@@ -0,0 +1,24 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type Pet struct {
+	gorm.Model
+	Name string
+	Status string
+	PhotoUrls []PhotoUrl
+}
+
+type PhotoUrl struct {
+	gorm.Model
+	PetId uint
+	Url string
+}
+
+type Tag struct {
+	gorm.Model
+	PetId uint
+	Name string
+}
